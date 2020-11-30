a694fa6cbce0891f32c038cab2464b517a1ea762
Kevin Glover <kevin.glover@nottingham.ac.uk>
2020-11-30 14:55:58 +0000

    Hide

--- server/src/routes/chocolate.js
+++ server/src/routes/chocolate.js
@@ -141,11 +141,11 @@ router.get('/list/XyuWdahM55yCTyF8dxcK', async (req, res) => {
 // 			res.status(500).send(err);
 // 		});
 // });
-
-router.get('/api/logs', async (req, res) => {
-	const result = await req.app.locals.chocDb.collection('log').find().toArray()
-	res.json(result);
-});
+//
+// router.get('/api/logs', async (req, res) => {
+// 	const result = await req.app.locals.chocDb.collection('log').find().toArray()
+// 	res.json(result);
+// });
 
 router.get('/gift/:orderid', async (req, res) => {
 	const orderid = req.params['orderid'];
@@ -239,17 +239,17 @@ router.post('/gift/:orderid/updateMessage', async (req, res) => {
 		res.status(404).send();
 	}
 });
-
-router.get('/gift/:orderid/delete/LEsWz26vmvSTwBXr9YMS', upload.single('file'), async (req, res) => {
-	const orderid = req.params['orderid'];
-	const order = await req.app.locals.chocDb.collection('gift').findOne({"order": orderid})
-	if (order) {
-		await req.app.locals.chocDb.collection('gift').deleteOne({"order": orderid})
-		res.status(200).send("success");
-	} else {
-		res.status(404).send();
-	}
-});
+//
+// router.get('/gift/:orderid/delete/LEsWz26vmvSTwBXr9YMS', upload.single('file'), async (req, res) => {
+// 	const orderid = req.params['orderid'];
+// 	const order = await req.app.locals.chocDb.collection('gift').findOne({"order": orderid})
+// 	if (order) {
+// 		await req.app.locals.chocDb.collection('gift').deleteOne({"order": orderid})
+// 		res.status(200).send("success");
+// 	} else {
+// 		res.status(404).send();
+// 	}
+// });
 
 router.post('/gift/:orderid/addFile', upload.single('file'), async (req, res) => {
 	const orderid = req.params['orderid'];
