8fcbc105cae611098d6d194ae8930bba30db00f4
Kevin Glover <kevin.glover@nottingham.ac.uk>
2021-02-22 16:01:05 +0000

    Move artcodes to packages

---
+++ src/routes/wetlands/artcodes.svelte
@@ -0,0 +1,19 @@
+<script context="module" lang="ts">
+</script>
+
+<svelte:head>
+	<title>Wetlands</title>
+	<link rel="stylesheet" href="/leaflet.css"/>
+</svelte:head>
+
+<div class="w-full" style="height: 800px" ></div>
+
+<script lang="ts">
+	//export let plot;
+
+	export async function mapSetup(elem: HTMLElement) {
+		const {createScanner, ScannerUI} = await import('artcodes-js');
+		const ui = new ScannerUI();
+
+	}
+</script>
