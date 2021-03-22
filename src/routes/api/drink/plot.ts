a6b4b4818fd709c8c8f0fb6684013af66655ce90
Kevin Glover <kevin.glover@nottingham.ac.uk>
2021-03-22 14:59:47 +0000

    Stuff

--- src/routes/api/drink/plot.ts
+++ src/routes/api/drink/plot.ts
@@ -2,16 +2,22 @@ import type {Request, Response} from "express";
 import simplify from "simplify-js";
 import {getMongoCollection} from "../../../shared/db";
 
+interface Point {
+	x: number,
+	y: number
+}
+
 export async function get(req: Request, res: Response) {
 	const result = await getMongoCollection(req, 'drink').find().sort({"device": 1, "time": 1}).toArray();
 
 	let lines = []
-	let dataAccel = []
-	let dataAccelDrink = []
+	let dataAccel: Array<Point> = []
+	let dataAccelDrink: Array<Point> = []
 	let prev = {device: '', tag: null}
 
 	result.push({x: 0, device: "end"})
 	result.forEach((item) => {
+		console.log(item)
 		if ('x' in item) {
 			if (item.device != prev.device) {
 				createLine(lines, dataAccelDrink, prev.device + " Drinking", '#282')
@@ -19,7 +25,7 @@ export async function get(req: Request, res: Response) {
 				dataAccel = []
 				dataAccelDrink = []
 			}
-			if (item.tag != prev.tag && dataAccel.length != 0) {
+			if (dataAccel.length > 0 && item.tag != prev.tag) {
 				dataAccel.push(convertItem(item))
 				dataAccelDrink.push(convertItem(item))
 			} else if (item.tag == 'drink') {
@@ -34,41 +40,43 @@ export async function get(req: Request, res: Response) {
 	res.json(lines)
 }
 
-function convertItem(item) {
+function convertItem(item): Point {
 	return {x: item.time, y: item.x + item.y + item.z}
 }
 
-function createLine(lines, data, name: string, colour: string) {
-	const lineSegments = []
-	let currentSegment = []
-	let prev = data[0].x
-	data.forEach((item) => {
-		if (Math.abs(prev - item.x) >= 1000) {
-			lineSegments.push(simplify(currentSegment, 0.02, false))
-			currentSegment = []
-		}
-		currentSegment.push(item)
-		prev = item.x
-	});
+function createLine(lines, data: Array<Point>, name: string, colour: string) {
+	if (data.length > 1) {
+		const lineSegments = []
+		let currentSegment = []
+		let prev = data[0].x
+		data.forEach((item) => {
+			if (Math.abs(prev - item.x) >= 1000) {
+				lineSegments.push(simplify(currentSegment, 0.02, false))
+				currentSegment = []
+			}
+			currentSegment.push(item)
+			prev = item.x
+		});
 
-	let line = {
-		x: [],
-		y: [],
-		mode: 'lines',
-		name: name,
-		line: {
-			color: colour,
-			width: 1
-		}
-	};
-	lineSegments.forEach((segment) => {
-		segment.forEach((point) => {
-			line.x.push(new Date(point.x).toISOString());
-			line.y.push(point.y);
+		let line = {
+			x: [],
+			y: [],
+			mode: 'lines',
+			name: name,
+			line: {
+				color: colour,
+				width: 1
+			}
+		};
+		lineSegments.forEach((segment) => {
+			segment.forEach((point) => {
+				line.x.push(new Date(point.x).toISOString());
+				line.y.push(point.y);
 
-		})
-		line.x.push(null);
-		line.y.push(null);
-	});
-	lines.push(line);
+			})
+			line.x.push(null);
+			line.y.push(null);
+		});
+		lines.push(line);
+	}
 }
