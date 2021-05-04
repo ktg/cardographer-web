a3b24d7ef32523c432e05ce714b997e792413685
Kevin Glover <kevin.glover@nottingham.ac.uk>
2021-05-04 14:18:29 +0100

    Add Etag to cors headers

--- src/server.ts
+++ src/server.ts
@@ -27,10 +27,13 @@ const upload = multer({
 	})
 });
 
-app.use(['/api/', '/images/'], cors());
-app.use(express.static('static'));
+app.use('/api/', cors())
+app.use('/images/', cors({
+	exposedHeaders: ['ETag']
+}))
+app.use(express.static('static'))
 app.use(uploadUri, express.static(uploadDir))
-app.use(express.json());
+app.use(express.json())
 app.use('/api/wetlands/sightings', upload.single('image'))
 app.use(session({
 	secret: 'cardiosquirrel',
