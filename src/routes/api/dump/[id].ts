2a47af7ff0c34e5333828bcc35eddcfea036a4fa
Kevin Glover <kevin.glover@nottingham.ac.uk>
2021-06-21 09:50:48 +0100

    Fixes

--- src/routes/api/dump/[id].ts
+++ src/routes/api/dump/[id].ts
@@ -1,12 +1,14 @@
 import {getDb} from "$lib/db";
 import type {EndpointOutput, Request} from "@sveltejs/kit";
-import {ObjectId} from 'mongodb';
+import mongodb from "mongodb";
+// commonjs workaround? (build for node complains)
+let {ObjectID} = mongodb;
 
 export async function get(req: Request): Promise<EndpointOutput> {
 	const {id} = req.params;
 
 	const db = await getDb()
-	const result = await db.collection('dump').findOne({'_id': new ObjectId(id)});
+	const result = await db.collection('dump').findOne({'_id': new ObjectID(id)});
 	if (result !== null) {
 		return {body: result}
 	} else {
