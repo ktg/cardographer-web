86ac685d9f4d01535c421440a2b968e10aa4c7e9
Kevin Glover <kevin.glover@nottingham.ac.uk>
2021-06-21 12:33:42 +0100

    Fix uploads?

--- src/routes/api/dump/index.ts
+++ src/routes/api/dump/index.ts
@@ -2,7 +2,7 @@ import {getDb} from "$lib/db";
 import type {EndpointOutput, Request} from "@sveltejs/kit"
 
 export async function post(req: Request): Promise<EndpointOutput> {
-	let dumpDoc = req.body
+	let dumpDoc = JSON.parse(req.rawBody as string)
 	const db = await getDb()
 	const result = await db.collection('dump').insertOne(dumpDoc)
 	return {body: {"result": "success", "insertedId": result.insertedId}}
--- src/routes/api/xSpace/index.ts
+++ src/routes/api/xSpace/index.ts
@@ -2,7 +2,7 @@ import {getDb} from "$lib/db";
 import type {EndpointOutput, Request} from "@sveltejs/kit";
 
 export async function post(req: Request): Promise<EndpointOutput> {
-	let dumpDoc = req.body
+	let dumpDoc = JSON.parse(req.rawBody as string)
 	const db = await getDb()
 	await db.collection('xSpace').insertMany(dumpDoc)
 	return {body: {"result": "success"}}
