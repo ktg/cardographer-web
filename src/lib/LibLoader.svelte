452b22b540760331172cdb6f663f7a1f25ce109c
Kevin Glover <kevin.glover@nottingham.ac.uk>
2021-07-22 11:48:42 +0100

    Update LibLoader.svelte

--- src/lib/LibLoader.svelte
+++ src/lib/LibLoader.svelte
@@ -1,12 +1,15 @@
 <script>
-	import { onMount, createEventDispatcher } from 'svelte';
+	import {createEventDispatcher, onMount} from 'svelte';
 
 	const dispatch = createEventDispatcher();
 	export let url;
 	let script;
 
 	onMount(async () => {
+		console.log("lib1")
 		script.addEventListener('load', () => {
+			console.log("lib2")
+
 			dispatch('loaded');
 		})
 
@@ -15,8 +18,14 @@
 			dispatch('error');
 		});
 	});
+
+	function loaded() {
+		console.log("lib3")
+
+		dispatch('loaded');
+	}
 </script>
 
 <svelte:head>
-	<script bind:this={script} src={url} />
+	<script bind:this={script} src={url} on:load={loaded}/>
 </svelte:head>
